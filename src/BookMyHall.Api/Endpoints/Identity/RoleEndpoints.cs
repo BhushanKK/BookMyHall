9b704f2f6fed879ca4a03aa07db859ff3abb0cb2
Bhushan KK <bhushankachave9900@gmail.com>
2026-07-31 10:00:50 +0530

    Added athorization

--- src/BookMyHall.Api/Endpoints/Identity/RoleEndpoints.cs
+++ src/BookMyHall.Api/Endpoints/Identity/RoleEndpoints.cs
@@ -9,7 +9,7 @@ public static class RoleEndpoints
     public static void MapRoleEndpoints(this IEndpointRouteBuilder app)
     {
         var group = app.MapGroup("/api/roles")
-            .WithTags("Roles");
+            .RequireAuthorization();
 
         group.MapPost("/", async (
             CreateRoleCommand command,
