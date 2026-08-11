54dd63ca8a14bd4a13ba2125a9853bf1568464a0
Bhushan KK <bhushankachave9900@gmail.com>
2026-08-11 07:44:55 +0530

    add jsonIgnore

--- src/BookMyHall.Application/Features/Identity/Authentication/Common/LoginRequest.cs
+++ src/BookMyHall.Application/Features/Identity/Authentication/Common/LoginRequest.cs
@@ -1,11 +1,17 @@
+using System.Text.Json.Serialization;
+
 namespace BookMyHall.Application.Features.Identity.Authentication;
 
 public sealed class LoginRequest
 {
     public string MobileNumber { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
+    [JsonIgnore]
     public string DeviceIdentifier { get; set; } = string.Empty;
+    [JsonIgnore]
     public string? DeviceName { get; set; }
+    [JsonIgnore]
     public string? PushNotificationToken { get; set; }
+    [JsonIgnore]
     public string? AppVersion { get; set; }
 }
