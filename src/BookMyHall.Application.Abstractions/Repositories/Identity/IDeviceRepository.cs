00fc64707ccbd78599b7d64f283b88db0ba6ca9c
PriyaaPote <priyankapote1313@gmail.com>
2026-08-15 18:52:37 +0530

    Added Query in Device Api

--- src/BookMyHall.Application.Abstractions/Repositories/Identity/IDeviceRepository.cs
+++ src/BookMyHall.Application.Abstractions/Repositories/Identity/IDeviceRepository.cs
@@ -1,3 +1,4 @@
+using BookMyHall.Contracts.Common;
 using BookMyHall.Domain.Identity;
 
 namespace BookMyHall.Application.Abstractions.Persistence.Repositories;
@@ -7,4 +8,6 @@ public interface IDeviceRepository
     Task<Device?> GetByDeviceIdentifierAsync(Guid userId, string deviceIdentifier, CancellationToken cancellationToken);
     Task AddAsync(Device device, CancellationToken cancellationToken);
     Task UpdateAsync(Device device, CancellationToken cancellationToken);
+    Task<PaginatedResult<Device>> GetAllAsync(PaginationRequest request,CancellationToken cancellationToken= default);
+    Task<Device?> GetByIdAsync(Guid deviceId,CancellationToken cancellationToken=default);
 }
