80c3e1044e80dc2e41f1f3c81fcaf7d42d13bdef
Bhushan KK <bhushankachave9900@gmail.com>
2026-08-15 21:37:54 +0530

    added in memory secrete key code in test case.

--- tests/BookMyHall.Api.Tests/BookMyHallWebApplicationFactory.cs
+++ tests/BookMyHall.Api.Tests/BookMyHallWebApplicationFactory.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 
@@ -6,9 +7,12 @@ namespace BookMyHall.Api.Tests;
 public class BookMyHallWebApplicationFactory
     : WebApplicationFactory<Program>
 {
-    protected override void ConfigureWebHost(
-        Microsoft.AspNetCore.Hosting.IWebHostBuilder builder)
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseSetting(
+            "Jwt:SecretKey",
+            "BookMyHall-Test-Secret-Key-For-Automated-Tests-Only-123456789");
+
         builder.ConfigureAppConfiguration((context, config) =>
         {
             config.AddInMemoryCollection(new Dictionary<string, string?>
