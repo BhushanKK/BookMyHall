d15cebea2186d091c4ad57869f90a6e764adcadd
Bhushan KK <bhushankachave9900@gmail.com>
2026-08-20 09:18:37 +0530

    make Nullable Gender and DateOfBirth Field of User Entity.

--- src/BookMyHall.Domain/Entities/Identity/User.cs
+++ src/BookMyHall.Domain/Entities/Identity/User.cs
@@ -14,8 +14,8 @@ public class User : BaseEntity
     public string EmailAddress { get; set; } = string.Empty;
     public string PasswordHash { get; set; } = string.Empty;
     public string? ProfileImageUrl { get; set; }
-    public DateTimeOffset DateOfBirth { get; set; }
-    public Gender Gender { get; set; }
+    public DateTimeOffset? DateOfBirth { get; set; }
+    public Gender? Gender { get; set; }
     public bool IsMobileVerified { get; set; }
     public bool IsEmailVerified { get; set; }
     public bool IsActive { get; set; } = true;
