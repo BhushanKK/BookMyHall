22d5c23e1e80e9fbc94bfbbc6cefac051a33e78b
Bhushan KK <bhushankachave9900@gmail.com>
2026-08-23 20:28:26 +0530

    fix create issue.

--- src/BookMyHall.Application/Features/Identity/Menu/Handlers/CreateMenuCommandHandler.cs
+++ src/BookMyHall.Application/Features/Identity/Menu/Handlers/CreateMenuCommandHandler.cs
@@ -1,7 +1,8 @@
-using MediatR;
 using System.Net;
 using AutoMapper;
 using FluentValidation;
+using MediatR;
+using BookMyHall.Application.Abstractions.Caching;
 using BookMyHall.Application.Abstractions.Persistence;
 using BookMyHall.Application.Abstractions.Persistence.Repositories;
 using BookMyHall.Contracts.Common;
@@ -9,7 +10,6 @@ using BookMyHall.Domain.Entities.Identity;
 using BookMyHall.Persistence.Exceptions;
 using BookMyHall.Shared.Common;
 using BookMyHall.Shared.Constants;
-using BookMyHall.Application.Abstractions.Caching;
 
 namespace BookMyHall.Application.Features.Identity;
 
@@ -18,42 +18,63 @@ public sealed class CreateMenuCommandHandler(
     IUnitOfWork unitOfWork,
     IMapper mapper,
     IValidator<CreateMenuCommand> validator,
-    IMessageHelper messageHelper,ICacheService cacheService)
+    IMessageHelper messageHelper,
+    ICacheService cacheService)
     : IRequestHandler<CreateMenuCommand, ApiResponse<MenuDto>>
 {
     public async Task<ApiResponse<MenuDto>> Handle(CreateMenuCommand request, CancellationToken cancellationToken)
     {
-        var validationResult = await validator.ValidateAsync(request,cancellationToken);
+        var validationResult = await validator.ValidateAsync(request, cancellationToken);
 
         if (!validationResult.IsValid)
         {
-            var message = string.Join("|",validationResult.Errors.Select(x => x.ErrorMessage));
+            var message = string.Join(" | ", validationResult.Errors.Select(x => x.ErrorMessage));
             return ApiResponse<MenuDto>.FailureResponse(message, HttpStatusCode.BadRequest);
+        }
+
+        Menu? parentMenu = null;
+
+        if (request.ParentMenuId.HasValue)
+        {
+            parentMenu = await menuRepository.GetByIdAsync(request.ParentMenuId.Value, cancellationToken);
+
+            if (parentMenu is null)
+            {
+                return ApiResponse<MenuDto>.FailureResponse
+                (
+                    messageHelper.NotFoundEntity(ResourceNames.Entities, EntityKeys.Menu),
+                    HttpStatusCode.NotFound
+                );
+            }
         }
 
         var menu = mapper.Map<Menu>(request);
 
+        menu.Level = parentMenu is null
+        ? (short)1
+        : (short)(parentMenu.Level + 1);
+
         try
         {
-            await menuRepository.AddAsync(menu,cancellationToken);
+            await menuRepository.AddAsync(menu, cancellationToken);
             await unitOfWork.SaveChangesAsync(cancellationToken);
         }
-        catch(DuplicateRecordException)
+        catch (DuplicateRecordException)
         {
             return ApiResponse<MenuDto>.FailureResponse
             (
-                messageHelper.AlreadyExistsEntity(ResourceNames.Entities,EntityKeys.Menu),
+                messageHelper.AlreadyExistsEntity(ResourceNames.Entities, EntityKeys.Menu),
                 HttpStatusCode.Conflict
             );
         }
-        await cacheService.RemoveByPrefixAsync(CacheKeys.Menus,cancellationToken);
-        
+
+        await cacheService.RemoveByPrefixAsync(CacheKeys.Menus, cancellationToken);
+
         return ApiResponse<MenuDto>.SuccessResponse
         (
             mapper.Map<MenuDto>(menu),
-            messageHelper.AddedEntity(ResourceNames.Entities,EntityKeys.Menu),
+            messageHelper.AddedEntity(ResourceNames.Entities, EntityKeys.Menu),
             HttpStatusCode.Created
         );
     }
-    
 }
