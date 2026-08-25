21b33cfd11865ed98deefbb1ba2c2328733f5244
Bhushan KK <bhushankachave9900@gmail.com>
2026-08-25 06:33:43 +0530

    fix state active inactive issue.

--- src/BookMyHall.Application/Features/Master/States/Dtos/StateDto.cs
+++ src/BookMyHall.Application/Features/Master/States/Dtos/StateDto.cs
@@ -6,8 +6,8 @@ public class StateDto
 {
     [JsonIgnore]
     public Guid StateId { get; set; }
-    public Guid CountryId { get; init; }
-    public string StateName { get; init; } = string.Empty;
-    public string StateCode {get; init;}=string.Empty;
-    public bool IsActive { get; init; }
+    public Guid CountryId { get; set; }
+    public string StateName { get; set; } = string.Empty;
+    public string StateCode {get; set;}=string.Empty;
+    public bool IsActive { get; set; }
 }
