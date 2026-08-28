0cb346c1457a2c3484cf26afb7c33d9202491fcc
Bhushan KK <bhushankachave9900@gmail.com>
2026-08-28 18:05:59 +0530

    fix user api.

--- src/BookMyHall.Application/Features/Identity/Users/Handlers/GetUsersQueryHandler.cs
+++ src/BookMyHall.Application/Features/Identity/Users/Handlers/GetUsersQueryHandler.cs
@@ -7,28 +7,33 @@ using BookMyHall.Shared.Constants;
 using BookMyHall.Application.Abstractions.Persistence.Repositories;
 using BookMyHall.Application.Abstractions.Caching;
 using BookMyHall.Domain.Entities.Identity;
+using BookMyHall.Application.Common.Interfaces.Storage;
 
 namespace BookMyHall.Application.Features.Identity.Users;
 
 public sealed class GetUsersQueryHandler(
     IUserRepository userRepository,
     IMapper mapper,
-    IMessageHelper messageHelper,ICacheService cacheService)
+    IMessageHelper messageHelper,
+    ICacheService cacheService,
+    IR2StorageService storageService)
     : IRequestHandler<GetUsersQuery, ApiResponse<PaginatedResponse<UserDto>>>
 {
     public async Task<ApiResponse<PaginatedResponse<UserDto>>> Handle(
         GetUsersQuery request,
         CancellationToken cancellationToken)
     {
-          var pagination = request.paginationRequest;
+        var pagination = request.paginationRequest;
 
-        var cacheKey = CacheKeyBuilder.BuildPaginatedKey<User>(
+        var cacheKey = CacheKeyBuilder.BuildPaginatedKey<User>
+        (
             CacheKeys.Users,
             pagination.PageNumber,
             pagination.PageSize,
             pagination.SearchText,
             pagination.SortBy,
-            pagination.SortDescending);
+            pagination.SortDescending
+        );
 
         var cachedResponse = await cacheService.GetAsync<PaginatedResponse<UserDto>>(cacheKey, cancellationToken);
 
@@ -41,18 +46,41 @@ public sealed class GetUsersQueryHandler(
                 HttpStatusCode.OK
             );
         }
-        var pagedResult = await userRepository.GetAllAsync(request.paginationRequest, cancellationToken);
+
+        var pagedResult = await userRepository.GetAllAsync(pagination, cancellationToken);
+
+        var users = mapper.Map<IReadOnlyList<UserDto>>(pagedResult.Items);
+        await Task.WhenAll
+        (
+            users.
+            Where(user => !string.IsNullOrWhiteSpace(user.ProfileImageUrl))
+            .Select(async user =>
+            {
+                user.ProfileImageUrl = await storageService.GetPreSignedUrlAsync
+                (
+                    user.ProfileImageUrl!,
+                    TimeSpan.FromDays(6).Add(TimeSpan.FromHours(23)),
+                    cancellationToken
+                );
+            })
+        );
 
         var response = new PaginatedResponse<UserDto>
         {
-            Items = mapper.Map<IReadOnlyList<UserDto>>(pagedResult.Items),
+            Items = users,
             PageNumber = pagedResult.PageNumber,
             PageSize = pagedResult.PageSize,
             TotalRecords = pagedResult.TotalCount
         };
 
-        await cacheService.SetAsync(cacheKey, response, TimeSpan.FromMinutes(30), cancellationToken);
-        
+        await cacheService.SetAsync
+        (
+            cacheKey,
+            response,
+            TimeSpan.FromMinutes(30),
+            cancellationToken
+        );
+
         return ApiResponse<PaginatedResponse<UserDto>>.SuccessResponse
         (
             response,
