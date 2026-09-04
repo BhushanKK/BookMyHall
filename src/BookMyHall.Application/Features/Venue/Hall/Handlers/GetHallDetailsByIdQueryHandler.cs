5cc8e2a6fdccec600d39c8ec94a2bfdd1178a461
Bhushan KK <bhushankachave9900@gmail.com>
2026-09-04 22:31:04 +0530

    added pre-signed Image Url

--- src/BookMyHall.Application/Features/Venue/Hall/Handlers/GetHallDetailsByIdQueryHandler.cs
+++ src/BookMyHall.Application/Features/Venue/Hall/Handlers/GetHallDetailsByIdQueryHandler.cs
@@ -1,16 +1,18 @@
 using System.Net;
 using MediatR;
-using BookMyHall.Application.Abstractions.Persistence.Repositories;
 using BookMyHall.Contracts.Common;
 using BookMyHall.Shared.Common;
 using BookMyHall.Shared.Constants;
 using BookMyHall.Domain.Venue;
+using BookMyHall.Application.Common.Interfaces.Storage;
+using BookMyHall.Application.Abstractions.Persistence.Repositories;
 
 namespace BookMyHall.Application.Features.Venue;
 
 public sealed class GetHallDetailsByIdQueryHandler(
     IHallRepository hallRepository,
-    IMessageHelper messageHelper)
+    IMessageHelper messageHelper,
+    IR2StorageService storageService)
     : IRequestHandler<GetHallDetailsByIdQuery, ApiResponse<HallListView>>
 {
     public async Task<ApiResponse<HallListView>> Handle(
@@ -26,6 +28,20 @@ public sealed class GetHallDetailsByIdQueryHandler(
             return ApiResponse<HallListView>.FailureResponse(
                 messageHelper.NotFound(EntityKeys.Hall),
                 HttpStatusCode.NotFound);
+        }
+
+        // =========================================================
+        // Generate pre-signed URL for cover image
+        // =========================================================
+
+        if (!string.IsNullOrWhiteSpace(hall.CoverImageUrl))
+        {
+            hall.CoverImageUrl =
+                await storageService.GetPreSignedUrlAsync(
+                    hall.CoverImageUrl,
+                    TimeSpan.FromDays(6).Add(
+                        TimeSpan.FromHours(23)),
+                    cancellationToken);
         }
 
         return ApiResponse<HallListView>.SuccessResponse(
