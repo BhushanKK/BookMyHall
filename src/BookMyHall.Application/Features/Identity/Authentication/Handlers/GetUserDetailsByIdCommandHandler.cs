c5b369ef1d586218e0efcf45cc72052ac7cde6eb
Bhushan KK <bhushankachave9900@gmail.com>
2026-09-12 09:25:30 +0530

    fix pre-signed url.

--- src/BookMyHall.Application/Features/Identity/Authentication/Handlers/GetUserDetailsByIdCommandHandler.cs
+++ src/BookMyHall.Application/Features/Identity/Authentication/Handlers/GetUserDetailsByIdCommandHandler.cs
@@ -5,24 +5,50 @@ using BookMyHall.Shared.Common;
 using BookMyHall.Shared.Constants;
 using BookMyHall.Domain.Identity;
 using BookMyHall.Application.Abstractions.Persistence.Repositories;
+using BookMyHall.Application.Common.Interfaces.Storage;
 
 namespace BookMyHall.Application.Features.Identity;
 
-public sealed class GetUserDetailsByIdQueryHandler(IUserRepository userRepository,IMessageHelper messageHelper)
-    : IRequestHandler<GetUserDetailsByIdQuery,ApiResponse<UserDetailsView>>
+public sealed class GetUserDetailsByIdQueryHandler(
+    IUserRepository userRepository,
+    IMessageHelper messageHelper,
+    IR2StorageService storageService)
+    : IRequestHandler<GetUserDetailsByIdQuery, ApiResponse<UserDetailsView>>
 {
-    public async Task<ApiResponse<UserDetailsView>>Handle(GetUserDetailsByIdQuery request,CancellationToken cancellationToken)
+    public async Task<ApiResponse<UserDetailsView>> Handle(
+        GetUserDetailsByIdQuery request,
+        CancellationToken cancellationToken)
     {
-        var userDetails =await userRepository.GetUserDetailsByIdAsync(request.userId,request.roleId,cancellationToken);
+        var userDetails = await userRepository.GetUserDetailsByIdAsync
+        (
+            request.userId,
+            request.roleId,
+            cancellationToken
+        );
+
         if (userDetails is null)
         {
-            return ApiResponse<UserDetailsView>.FailureResponse(
+            return ApiResponse<UserDetailsView>.FailureResponse
+            (
                 messageHelper.NotFound(EntityKeys.User),
-                HttpStatusCode.NotFound);
+                HttpStatusCode.NotFound
+            );
         }
 
-        return ApiResponse<UserDetailsView>.SuccessResponse(userDetails,
-            messageHelper.RetrievedEntity(ResourceNames.Entities,EntityKeys.User),
-            HttpStatusCode.OK);
+        if (!string.IsNullOrWhiteSpace(userDetails.ProfileImageUrl))
+        {
+            userDetails.ProfileImageUrl = await storageService.GetPreSignedUrlAsync
+            (
+                userDetails.ProfileImageUrl,
+                TimeSpan.FromDays(6).Add(TimeSpan.FromHours(23)),cancellationToken
+            );
+        }
+
+        return ApiResponse<UserDetailsView>.SuccessResponse
+        (
+            userDetails,
+            messageHelper.RetrievedEntity(ResourceNames.Entities, EntityKeys.User),
+            HttpStatusCode.OK
+        );
     }
 }
